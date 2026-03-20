f18a5c2788d6ee0c18bc8057ff2a066309d90980
Igor Ricardo <122498648+IgorRicardo1@users.noreply.github.com>
2026-03-20 14:57:39 -0300

    fix: allow all vercel subdomains in CORS

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    // Permite localhost, o domínio principal ou QUALQUER subdomínio da Vercel do seu projeto
+    if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
       callback(null, true);
     } else {
       callback(new Error('Bloqueado pelo CORS: Acesso não autorizado.'));
